8936fc7ed0e51334bf793f1bd435de0f7270a677
Djumabaevs <djumabaevb@gmail.com>
2021-06-13 13:46:29 +0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/viewpager2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.viewpager2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/viewpager2/ScreenSlidePageFragment.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.viewpager2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+
+}
